ae5e3beba3c2ea333843b321805813e36e2b9042
Ahmed Hassanien <me@ahmedhassanien.com>
2019-10-26 21:44:50 +0200

    removed the idea of shell scripts

--- 00_ohMyZsh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Installing Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Installing Powerlevel10k
-git clone https://github.com/romkatv/powerlevel10k.git ${HOME}/.oh-my-zsh/custom/themes/powerlevel10k
-sed -i -e 's/ZSH_THEME.*/ZSH_THEME=powerlevel10k\/powerlevel10k/g' ~/.zshrc
-cp .p10k.zsh ~/.p10k.zsh
